79c8808ff09dbd97d8cb8089648495aa5c1bb740
Vishnupriya <vishnupriya.t7@gmail.com>
2023-03-27 16:24:08 +0530

    home page modified

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import React from "react";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import About from "./components/About";
-import Services from "./components/Services"
+import Services from "./components/Services";
 import Skills from "./components/Skills";
-import {Container} from 'react-bootstrap'
+import { Container } from "react-bootstrap";
 import Contact from "./components/Contact";
 // import Footer from "./components/Footer";
 
@@ -13,14 +13,13 @@ function App() {
   return (
     <>
       <Header />
-     <Container fluid className="p-0">
+      <Container fluid className="ps-0">
         <Home />
         <About />
         <Services />
         <Skills />
         <Contact />
       </Container>
-    
     </>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,8 @@ import "../styles/Home.css";
 import { Button, Row, Container, Col } from "react-bootstrap";
 import TypeWriterEffect from "react-typewriter-effect";
 import { GitHub, Facebook, LinkedIn } from "@mui/icons-material";
+// import { Link } from "@mui/material";
+import { IconButton } from "@mui/material";
 
 function Home() {
   return (
@@ -12,7 +14,7 @@ function Home() {
           <Col>
             <h6>FULL STACK DEVELOPER</h6>
             <h1>Hey !</h1>
-            
+
             <h2>
               <TypeWriterEffect
                 text="I am Vishnu Priya T"
@@ -26,22 +28,38 @@ function Home() {
             </span>
             <br />
             <br />
-            <Button size="lg" variant="primary">HIRE Me</Button> 
+            <Button size="lg" variant="primary">
+              HIRE Me
+            </Button>
             <br />
             <br />
             <span className="follow">Follow Me:</span>
             <br />
             <br />
             <div className="social-media">
-              <GitHub href="https://github.com/vishnupriyat7/React-portfolio" target="_blank" fontSize="large" color="primary" />
-              <LinkedIn href="https://www.linkedin.com/in/vishnupriyat/" target="_blank" fontSize="large" color="primary" />
-              <Facebook href="#" fontSize="large" color="primary" />
+              <IconButton
+                href="https://github.com/vishnupriyat7"
+                target="_blank"
+              >
+                <GitHub fontSize="large" color="primary" />
+              </IconButton>
+              <IconButton
+                href="https://www.linkedin.com/in/vishnupriyat/"
+                target="_blank"
+              >
+                <LinkedIn fontSize="large" color="primary" />
+              </IconButton>
+              <IconButton href="#"
+                target="_blank">
+                <Facebook href="#" fontSize="large" color="primary" />
+              </IconButton>
             </div>
           </Col>
           <Col>
-            {" "}<img style = {{width: '32rem', borderRadius: 20,  }}
+            {" "}
+            <img
+              style={{ width: "32rem", borderRadius: 20 }}
               src={require("../assets/software_img.jpg")}
-              
             />
           </Col>
         </Row>
